4475f9398a3f38696a37a353fe070cd6b341cdf9
yasoug <ougouisyassine@gmail.com>
2024-01-04 20:46:35 +0000

    commit 1

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Module for pascal_triangle function """
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal's triangle
+    Args:
+        n (int): number of rows
+    """
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
+    return triangle

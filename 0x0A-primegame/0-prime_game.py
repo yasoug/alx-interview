3cbd58753a935f94ccc9c59a1b755b791b93353a
yasoug <ougouisyassine@gmail.com>
2024-03-13 22:11:39 +0000

    commit 2

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -8,11 +8,13 @@ def isPrime(n):
         return False
     elif n <= 3:
         return True
-    i = 2
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
     while i * i <= n:
         if n % i == 0 or n % (i + 2) == 0:
             return False
-        i += 1
+        i += 6
     return True
 
 

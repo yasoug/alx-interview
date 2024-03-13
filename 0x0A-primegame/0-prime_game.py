942cf504b9a4047a67cb2fc0ecf693000089d863
yasoug <ougouisyassine@gmail.com>
2024-03-13 22:29:52 +0000

    commit 3

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,42 +5,32 @@
 def isPrime(n):
     """Looks for prime numbers"""
     if n <= 1:
-        return False
-    elif n <= 3:
-        return True
-    elif n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+        return []
 
+    primes = [True] * (n + 1)
+    primes[0] = primes[1] = False
 
-def countPrime(nums):
-    """Counts the prime numbers in the list"""
-    count = 0
-    for num in nums:
-        if isPrime(num):
-            count += 1
-    return count
+    p = 2
+    while p * p <= n:
+        for i in range(p * p, n + 1, p):
+            primes[i] = False
+        p += 1
+    return [i for i, is_prime in enumerate(primes) if is_prime]
 
 
 def isWinner(x, nums):
     """Determines who the winner is"""
-    Ben = 0
-    Maria = 0
-    if x <= 0 or not nums:
+    if x is None or nums is None or x == 0 or nums == []:
         return None
-    for num in range(x):
-        num_arr = [n for n in range(1, nums[num] + 1)]
-        if countPrime(num_arr) % 2 == 0:
+    Maria = Ben = 0
+    for i in range(x):
+        primes = isPrime(nums[i])
+        if len(primes) % 2 == 0:
             Ben += 1
         else:
             Maria += 1
     if Ben > Maria:
-        return "Ben"
-    if Ben == Maria:
-        return None
-    return "Maria"
+        return 'Ben'
+    elif Ben < Maria:
+        return 'Maria'
+    return None
